89c44b3aed1bfd32b297c4b1bab14cc11e48153b
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2015-12-05 00:30:42 +0000

    Day 1 - Part 2

--- solutions/1.js
+++ solutions/1.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var getRawInput = require('./helpers').getRawInput;
+var input = getRawInput(1);
 
 var BUTTON_VECTORS = {
   '(': 1,
@@ -26,4 +27,24 @@ assert.equal(pressAllButtons('))('), -1);
 assert.equal(pressAllButtons(')))'), -3);
 assert.equal(pressAllButtons(')())())'), -3);
 
-console.log(pressAllButtons(getRawInput(1)));
+function stepsTo(targetFloor) {
+  return function(buttonSequence) {
+    return buttonSequence
+      .split('')
+      .reduce(function(trip, button, index){
+        var newFloor = pressAButton(trip.currentFloor, button);
+        return {
+          currentFloor: newFloor,
+          floorReachedAt: trip.floorReachedAt || (newFloor === targetFloor ? index + 1 : 0)
+        }
+    }, { currentFloor: 0 }).floorReachedAt;
+  }
+}
+
+assert.equal(stepsTo(-1)(')'), 1);
+assert.equal(stepsTo(-1)('()())'), 5);
+
+console.log({
+  destination: pressAllButtons(input),
+  stepBeforeReachingBasement: stepsTo(-1)(input)
+});
